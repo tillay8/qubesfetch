aa327f2f5b6966d2201c9f9ec43ddd8e4235698c
Tilley <tilleyy8@gmail.com>
2024-12-27 23:56:05 -0700

    Create reminders.sh

---
+++ reminders.sh
@@ -0,0 +1,5 @@
+# Reminder of escape sequences for different sections of the qubes logo
+
+echo -e "\033[38;2;154;192;255mTop section  XXXXXXXXXXX 033[38;2;154;192;255m"
+echo -e "\033[38;2;53;117;217mBottom Right XXXXXXXXXXX 033[38;2;53;117;217m"
+echo -e "\033[38;2;99;161;255mBottom Left  XXXXXXXXXXX 033[38;2;99;161;255m"
